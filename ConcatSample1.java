fbc41a9a60d507d0a5046cf427ce24cc2f888dd5
yuki525 <entio777@gmail.com>
2022-01-08 14:02:19 +0000

    java

---
+++ ConcatSample1.java
@@ -0,0 +1,8 @@
+public class ConcatSample1 {
+  static public void main(String args[]){
+    String str1 = "文字列１";
+    String str2 = "文字列２";
+    String str = str1 + str2;
+    System.out.println("+演算子による文字列の連結 -> " + str);
+  }
+}
