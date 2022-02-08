459f15297587d51b9e20078252df8826fd0215a2
yuki525 <entio777@gmail.com>
2022-02-08 14:06:01 +0000

    c言語　学習

--- test5.c
+++ test5.c
@@ -4,7 +4,7 @@ void main(){
     int a;                              //  変数a（キーボードからの値を代入)
     int b;
     printf("a=");                       //  キーボードからの入力を促す
-    scanf("%d",&a);                     //  キーボードから一文字入力
+    scanf("%d",&a);
     printf("b=");
     scanf("%d",&b);
     printf("aの値は%dです。\n",a);        //  aの値を表示
